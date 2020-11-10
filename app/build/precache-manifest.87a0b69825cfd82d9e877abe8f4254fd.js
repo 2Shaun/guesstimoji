d3d3a6fbcf8895fa816da08e85fe4dd2f1b347ce
tomoshaughnessy <tom.w.oshaughnessy@gmail.com>
2020-11-09 21:08:10 -0500

    Restructure folders for dockerfiles

---
+++ app/build/precache-manifest.87a0b69825cfd82d9e877abe8f4254fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efdc8f7b0cae9f4c16a5173501b04d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34df98bc06f86dbbeedf",
+    "url": "./static/css/main.cc4f970a.chunk.css"
+  },
+  {
+    "revision": "294105b9ebb496ca1f07",
+    "url": "./static/js/2.efb5e859.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "./static/js/2.efb5e859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34df98bc06f86dbbeedf",
+    "url": "./static/js/main.097d55c7.chunk.js"
+  },
+  {
+    "revision": "424d8cdc0d04e69e5a0c",
+    "url": "./static/js/runtime-main.a77e21fc.js"
+  }
+]);
