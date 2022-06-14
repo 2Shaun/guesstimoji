6b03569cd9d7e982ad6a0956ba4c3423b18ab160
Tom O'Shaughnessy <tom.w.oshaughnessy@gmail.com>
2022-06-14 07:14:21 -0400

    Update backend with variables from documentation

--- azInfrastructure/backend.tf
+++ azInfrastructure/backend.tf
@@ -1,5 +1,7 @@
 terraform {
     backend "http" {
-      
+        address = var.gitlab_remote_state_address
+        username = var.gitlab_username
+        password = var.gitlab_access_token 
     }
 }
--- azInfrastructure/variables.tf
+++ azInfrastructure/variables.tf
@@ -6,9 +6,28 @@ variable "location" {
 variable "app" {
     type = string
     default = "guesstimoji"
+  description = "App and database name"
 }
 
 variable "collNames" {
   type = list(string)
   default = ["boards", "emojis", "games"]
+  description = "Collection names for the guesstimoji database"
+}
+
+variable "gitlab_remote_state_address" {
+  type = string
+  default = "https://gitlab.com/api/v4/projects/31299207/terraform/state/azInfrastructure"
+  description = "Gitlab remote state file address"
+}
+
+variable "gitlab_username" {
+  type = string
+  default = "gitlab-ci-token"
+  description = "Gitlab username to query remote state"
+}
+
+variable "gitlab_access_token" {
+  type = string
+  description = "GitLab access token to query remote state"
 }
