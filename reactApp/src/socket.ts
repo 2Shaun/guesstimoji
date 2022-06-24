5b52ea7a518d103d19321d69232cbcc8e5f83e86
Tom O'Shaughnessy <tom.w.oshaughnessy@gmail.com>
2022-06-24 12:10:01 -0400

    Add missing colon to api calls

--- reactApp/src/socket.ts
+++ reactApp/src/socket.ts
@@ -3,6 +3,7 @@ import io from 'socket.io-client';
 const gameApiUrl =
     (process.env.REACT_APP_PROTOCOL as string) +
     process.env.REACT_APP_HOST +
+    ':' +
     process.env.REACT_APP_GAME_API_PORT;
 
 let socket = io(gameApiUrl);
