04764895e5a416e918e26d77b6007b381c9ff4c8
Tom O'Shaughnessy <tom.w.oshaughnessy@gmail.com>
2022-06-25 04:00:40 -0400

    Remove GitOps auth variables from tf vars

--- azInfrastructure/variables.tf
+++ azInfrastructure/variables.tf
@@ -14,20 +14,3 @@ variable "collNames" {
   default = ["boards", "emojis", "games"]
   description = "Collection names for the guesstimoji database"
 }
-
-variable "gitlab_remote_state_address" {
-  type = string
-  default = "https://gitlab.com/api/v4/projects/31299207/terraform/state/azInfrastructure"
-  description = "Gitlab remote state file address"
-}
-
-variable "gitlab_username" {
-  type = string
-  default = "gitlab-ci-token"
-  description = "Gitlab username to query remote state"
-}
-
-variable "gitlab_access_token" {
-  type = string
-  description = "GitLab access token to query remote state"
-}
